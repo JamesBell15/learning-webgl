1f833fdd8816b21238b3112f4c2c812b3c018830
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-12-06 14:04:56 +0100

    Add whole pipeline to draw triangle

--- program.js
+++ program.js
@@ -1,2 +1,111 @@
+let canvas = document.querySelector("#c")
 
- var canvas = document.querySelector("#c")
+let gl = canvas.getContext("webgl2");
+
+if (!gl) {
+  // Incase browser doesn't support GL
+  const title = document.getElementById("title")
+  title.textContent = "No GL :("
+}
+
+const vcheck = document.getElementById("v")
+vcheck.textContent = vertexShaderSource
+const fcheck = document.getElementById("f")
+fcheck.textContent = fragmentShaderSource
+
+function createShader(gl, type, source) {
+  let shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+// Creates shaders
+let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+// Links shaders
+let program = createProgram(gl, vertexShader, fragmentShader);
+
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+var positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+// three 2d points
+var positions = [
+  0, 0,
+  0, 0.5,
+  0.7, 0,
+];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+var vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+var size = 2;          // 2 components per iteration
+var type = gl.FLOAT;   // the data is 32bit floats
+var normalize = false; // don't normalize the data
+var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+var offset = 0;        // start at the beginning of the buffer
+gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset)
+
+function resizeCanvasToDisplaySize(canvas) {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth  = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width  !== displayWidth ||
+                     canvas.height !== displayHeight;
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+
+  return needResize;
+}
+
+resizeCanvasToDisplaySize(gl.canvas);
+
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+// Clear the canvas
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Tell it to use our program (pair of shaders)
+gl.useProgram(program);
+
+// Bind the attribute/buffer set we want.
+gl.bindVertexArray(vao);
+
+var primitiveType = gl.TRIANGLES;
+var offset = 0;
+var count = 3;
+gl.drawArrays(primitiveType, offset, count);
