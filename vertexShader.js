2e70afe3fa0d7b01d25670a8f35bed192b512cda
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-12-06 14:04:05 +0100

    Add shaders

---
+++ vertexShader.js
@@ -0,0 +1,15 @@
+// Important that #version 300 es is first
+const vertexShaderSource = `#version 300 es
+
+  // an attribute is an input (in) to a vertex shader.
+  // It will receive data from a buffer
+  in vec4 a_position;
+
+  // all shaders have a main function
+  void main() {
+
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = a_position;
+  }
+`
